fa6c1118e119b1b24efc5c3149872acecf64e935
AbhinavTIS <abhinavproam@gmail.com>
2024-05-13 17:55:26 -0400

    fetch the weather api using the fetched city id

--- forecast.js
+++ forecast.js
@@ -1,14 +1,32 @@
-const key ='81vSPABSdboGiXbdDHNAMiNP9SMF4ALW'
+const key = "81vSPABSdboGiXbdDHNAMiNP9SMF4ALW";
 
-const getCity = async function(city) {
-    const baseURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?apikey=${key}&q=${city}`
+getWeather = async function(cityCode){
+    const base = `http://dataservice.accuweather.com/currentconditions/v1/${cityCode}`
+    const query = `?apikey=${key}`
+   
+    const  response =await  fetch(base+query)    
+    const data =await  response.json()
 
-    const response = await fetch(baseURL+query) 
-    const data = await response.json()
+    return data
+    console.log(data);
+}
 
-    console.log(data[0]['Key']);
-} 
+const getCity = async function (city) {
+  const baseURL =
+    "http://dataservice.accuweather.com/locations/v1/cities/search";
+  const query = `?apikey=${key}&q=${city}`;
 
+  const response = await fetch(baseURL + query);
+  const data = await response.json();
+
+  return data[0]["Key"];
+};
 
 getCity('london')
+.then(async (data) => {
+//   console.log(data);
+getWeather(data)
+
+})
+.catch(() => console.log("Error"));
+// console.log('hello');
