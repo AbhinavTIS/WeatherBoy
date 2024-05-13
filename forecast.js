58477246315472fa3af0b358983f2a54f9baf8fb
AbhinavTIS <abhinavproam@gmail.com>
2024-05-13 16:33:08 -0400

    fix the query

--- forecast.js
+++ forecast.js
@@ -2,7 +2,7 @@ const key ='81vSPABSdboGiXbdDHNAMiNP9SMF4ALW'
 
 const getCity = async function(city) {
     const baseURL = 'http://dataservice.accuweather.com/locations/v1/cities/search'
-    const query = `?${key}&q=${city}`
+    const query = `?apikey=${key}&q=${city}`
 
     const response = await fetch(baseURL+query) 
     const data = await response.json()
