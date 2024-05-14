9888d1b85112ff5aef738db6a5bb90dda3f419ce
AbhinavTIS <abhinavproam@gmail.com>
2024-05-14 16:33:13 -0400

    handled the fetchWeatherData promise

--- app.js
+++ app.js
@@ -1,21 +1,36 @@
 const cityForm = document.querySelector("form");
 console.log(cityForm);
 
-cityForm.addEventListener('submit',function(e) {
-  e.preventDefault()
-
-  const cityName = cityForm.city.value.trim()
-
-  fetchWeatherData(cityName);
-} )
-
-
-const fetchWeatherData= async function(cityName){
+const fetchWeatherData = async function (cityName) {
     try {
-        const cityCode = await getCity(cityName)
-        const weatherObject  = await getWeather(cityCode)
-        console.log(weatherObject);
+      const cityCode = await getCity(cityName);
+      const weatherObject = await getWeather(cityCode);
+        
+      return {
+        cityCode : cityCode,
+        weatherObject : weatherObject
+      }
+
     } catch (error) {
-        console.log('Error in fetching the apis :', error );
+      console.log("Error in fetching the apis :", error);
     }
-}
+  };
+
+
+cityForm.addEventListener("submit", function (e) {
+  e.preventDefault();
+
+  const cityName = cityForm.city.value.trim();
+
+  fetchWeatherData(cityName)
+  .then( (data) => {
+    console.log(data);
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+
+  cityForm.reset()
+});
+
+
